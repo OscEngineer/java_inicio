94480d223f334c54f3b2c674c09dbd0688759309
Os_Progammer <capoeiraoscar@yahoo.com.mx>
2022-10-22 21:01:48 -0500

    inicio de la clase empleado

---
+++ Uso_Empleado.java
@@ -0,0 +1,17 @@
+package poo;
+
+public class Uso_Empleado {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+// cuando hacemos todo en un solo fichero java que alberga todas las clases, solo debe existir un metodo public y solo un metodo main
+}
+
+class Empleado{
+	
+	
+	
+	
+}
